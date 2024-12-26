46a4d64b648649b6eb4b98dae401264a156b91e0
CoryPearl <cory.pearl99@gmail.com>
2024-12-25 21:27:26 -0500

    email update

--- server.js
+++ server.js
@@ -42,13 +42,21 @@ function generateCode() {
 
 //send email for 2fa
 function sendEmail(firstName, email) {
+  //   var transporter = nodemailer.createTransport({
+  //     host: process.env.EMAIL_HOST,
+  //     port: process.env.EMAIL_PORT,
+  //     secure: false,
+  //     auth: {
+  //       user: process.env.EMAIL_USER,
+  //       pass: process.env.EMAIL_PASSWORD,
+  //     },
+  //   });
+
   var transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: false,
+    service: "gmail",
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD,
+      user: "cory.pearl99@gmail.com",
+      pass: "zsth skbg zvma fwhe",
     },
   });
 
